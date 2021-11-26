4a4cb6366f9664aa8ad1e9e963365792da9761b0
Roman <afonasev@gmail.com>
2021-11-26 17:22:50 +0300

    Hometask 2_3 completed

---
+++ djangoshop/ordersapp/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from ordersapp.models import Order, OrderItem
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+
+
+class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(
+        label='цена',
+        required=False,
+        widget=forms.TextInput(attrs={'readonly': 'readonly'})
+    )
+
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
