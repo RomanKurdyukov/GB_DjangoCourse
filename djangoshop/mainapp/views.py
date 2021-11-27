b34196fd36fbaa7e10876a3cae7c7c1c1527117a
Roman <afonasev@gmail.com>
2021-11-27 13:25:29 +0300

    Hometask 2_4 completed

--- djangoshop/mainapp/views.py
+++ djangoshop/mainapp/views.py
@@ -22,11 +22,20 @@ def products(request, pk=None):
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.all().order_by('price')[:4]
+            products = Product.objects.filter(
+                is_active=True,
+                category__is_active=True,
+                quantity__gte=1
+            ).order_by('price')[:4]
             category = {'name': 'все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('price')[:4]
+            products = Product.objects.filter(
+                category__pk=pk,
+                is_active=True,
+                category__is_active=True,
+                quantity__gte=1
+            ).order_by('price')[:4]
 
         context = {
             'title': title,
